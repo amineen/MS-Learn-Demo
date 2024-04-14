04335b9d22dd63e58e5c54a31ae64139ed09eae0
Aaron Mineen <amineen64@hotmail.com>
2024-04-14 10:38:34 +0200

    Add Car struct and related functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![allow(unused)]
+
+#[derive(Debug)]
 struct Car {
     color: String,
     motor: Transmission,
@@ -18,6 +20,29 @@ enum Age {
     New,
     Used,
 }
+
+fn car_quality(miles: u32) -> (Age, u32) {
+    if miles > 0 {
+        return (Age::Used, miles);
+    }
+
+    (Age::New, miles)
+}
+
+fn car_factory(order: i32, miles: u32) -> Car {
+    let colors = ["Blue", "Green", "Red", "Silver"];
+    let color_index = order % 4;
+
+    let color = colors[color_index as usize];
+
+    Car {
+        color: color.to_string(),
+        motor: Transmission::Manual,
+        roof: true,
+        age: car_quality(miles),
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let car = car_factory(2, 0);
+    println!("{:?}", car);
 }
